29deb33d40ae1269a07a667c7b306ed5dc780211
DanielcoderX <danyalghlaby@gmail.com>
2023-09-04 18:17:01 +0330

    Add package comment and comments for exported variables and functions in resolve package.

--- resolve/hosts.go
+++ resolve/hosts.go
@@ -1,5 +1,8 @@
+// Package resolve provides DNS resolution and host file management functionality.
 package resolve
 
+// CheckHosts checks if a given domain exists in the local resolver's hosts file
+// and returns the corresponding IP address if found, or an empty string if not.
 func (lr *LocalResolver) CheckHosts(domain string) string {
 	for h := range lr.Hosts {
 		if lr.Hosts[h].Domain == domain {
