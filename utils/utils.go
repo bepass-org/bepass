e6ea2a5b6a21a6d3fb8d1e41bdf7049d8530c751
uoosef <ghobadi@yekaan.ir>
2023-09-07 11:51:37 +0330

    add an interface for buffer reader

--- utils/utils.go
+++ utils/utils.go
@@ -2,6 +2,7 @@ package utils
 
 import (
 	"crypto/rand"
+	"io"
 )
 
 var chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890-"
@@ -14,4 +15,19 @@ func ShortID(length int) string {
 		b[i] = chars[int(b[i])%ll]
 	}
 	return string(b)
+}
+
+type BufferedReader struct {
+	FirstPacketData []byte
+	BufReader       io.Reader
+	FirstTime       bool
+}
+
+func (r *BufferedReader) Read(p []byte) (int, error) {
+	if r.FirstTime {
+		r.FirstTime = false
+		n := copy(p, r.FirstPacketData)
+		return n, nil
+	}
+	return r.BufReader.Read(p)
 }
